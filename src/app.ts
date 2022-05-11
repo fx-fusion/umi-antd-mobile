5bc7e04ae225056aa2a73dc2efc6524eb20855d4
god <oh@my.god>
2022-05-11 15:44:17 +0800

    change

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,9 @@
 import 'antd-mobile/es/global';
 import './theme.less';
+/**
+ * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
+ * */
+export async function getInitialState(): Promise<any> {
+  console.log('[getInitialState]');
+  return {};
+}
